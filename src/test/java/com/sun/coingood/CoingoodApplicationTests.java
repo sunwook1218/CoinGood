cf8970f5ad16adadc2cfe11cc7dde50c6eee5814
sunwook1218 <sunwook1218@naver.com>
2021-03-04 19:44:35 +0900

    Coin good init with start.spring.io

---
+++ src/test/java/com/sun/coingood/CoingoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.coingood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoingoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
